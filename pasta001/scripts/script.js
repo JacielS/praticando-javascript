ab8be587e1bfd946f40aeddcae0587e9f2b4ce56
Jaciel Caitité <98497533+JacielS@users.noreply.github.com>
2022-03-04 23:10:46 -0300

    mudanças

---
+++ pasta001/scripts/script.js
@@ -0,0 +1,11 @@
+
+
+function mudar(){
+    document.getElementById("demo").innerHTML="Hello JavaScript!"
+}
+function acender(){
+    document.getElementById("minha imagem").src="imagens/pic_bulbon.gif"
+}
+function apagar(){
+    document.getElementById("minha imagem").src="imagens/pic_bulboff.gif"
+}
